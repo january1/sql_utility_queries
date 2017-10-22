e42608063ef155f334b98e6ef8d68d85d80b7072
January Tabaka <january.tabaka@gmail.com>
2017-10-22 12:23:20 +0200

    Added query to see a total size of all segments by a specified owner.

--- physical_structures_queries.sql
+++ physical_structures_queries.sql
@@ -1,6 +1,12 @@
 SELECT segment_name
      , bytes/1024 AS size_kb
      , blocks
-  FROM dba_segments WHERE owner = :owner
+  FROM dba_segments
+ WHERE owner = :owner
  ORDER BY size_kb DESC
+;
+
+SELECT SUM(ROUND(bytes/1048576,2)) AS size_mb
+  FROM DBA_SEGMENTS
+ WHERE owner = :owner
 ;
